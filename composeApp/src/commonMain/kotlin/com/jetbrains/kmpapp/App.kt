e1ac877ffae71851e405d33258b32faa2814bfd2
Abdul Basit <abdulbasit5361234@gmail.com>
2024-06-16 17:45:25 +0500

    Remove unused screens

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/App.kt
@@ -2,12 +2,11 @@ package com.jetbrains.kmpapp
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import cafe.adriel.voyager.navigator.Navigator
-import com.jetbrains.kmpapp.screens.list.ListScreen
 
 @Composable
 fun App() {
     MaterialTheme {
-        Navigator(ListScreen)
+
+
     }
 }
